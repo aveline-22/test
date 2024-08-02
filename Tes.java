d7316095d95f70c971ebaa90779063b9a8fe2957
aveline-22 <aveline_wardani_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:59:08 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,6 @@
+public class Tes {
+    public static void main(String[] args) {
+        System.out.println("nama saya aveline dari kelas 10 rpl 7 yang berasal dari kota tulungagung");
+        String a = "1";
+    }
+}
