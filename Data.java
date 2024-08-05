5c8cbecbb91907eabf31303d0a0337064e22ad90
aveline-22 <aveline_wardani_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:38:45 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,16 @@
+public class Data {
+    public static void main(String[] args) {
+        //tipe_data nama_var = value/isi var;
+        String nama = "Aveline"; 
+        int usia = 16;
+        String alamat = "Jl. Danau Buyan";
+        double kelas = 10.7; 
+
+        System.out.println("nama saya: " + nama);
+        System.out.println("umur saya: " + usia + " tahun");
+        System.out.println("alamat saya: " + alamat);
+        System.out.println("kelas: " + kelas);
+
+
+    }
+}
