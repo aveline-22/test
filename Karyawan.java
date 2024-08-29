b7810701acd3bbc793f04c3891c0a919a421e129
aveline-22 <aveline_wardani_33rpl@student.smktelkom-mlg.sch.id>
2024-08-29 14:43:47 +0700

    Add files via upload

--- Karyawan.java
+++ Karyawan.java
@@ -5,8 +5,40 @@ public class Karyawan{
     String unit;
     String alamat;
 
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getUnit() {
+        return this.unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getAlamat() {
+        return this.alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
     //Contructor Parameter
-    public Karyawan (int id, String nama,String unit, String alamat ) {
+    public Karyawan (int id, String nama, String unit, String alamat ) {
         this.id = id;
         this.nama = nama;
         this.unit = unit;
