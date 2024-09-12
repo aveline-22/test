0d32326563fb1b232fa567aa57ffa537d03f607e
aveline-22 <aveline_wardani_33rpl@student.smktelkom-mlg.sch.id>
2024-09-12 15:00:41 +0700

    Add files via upload

--- CD.java
+++ CD.java
@@ -1,37 +1,64 @@
- package PERSEWAAN; // nama folder tempat penyimpanan
-
+package PERSEWAAN; // nama folder tempat penyimpanan
 
 //subclass
-    public class CD extends Product {
-      public static void main(String[] args) {
-         
-      }
-      private String artist;
-      private int totalSong;
-      private String label;
+public class CD extends Product {
 
-      public CD(){
-         number = 10;
-         name = "CD Selamat Siang";
-         quantity = 0;
-         artist = "Salman";
-         totalSong = 0;
-         label = "sony music";
+   private String artist;
+   private int totalSong;
+   private String label;
 
-         
-      }
-      public void print(){
-         System.out.println("Number : " + number);
-         System.out.println("Name : " + name);
-         System.out.println("Quantitiy : " + quantity);
-         System.out.println("Artist: " + artist);
-         System.out.println("Total song : " + totalSong);
-         System.out.println("Label : " + label);
-      }
+   public String getArtist() {
+      return this.artist;
+   }
 
-      
-      }
-    
-        
-    
+   public void setArtist(String artist) {
+      this.artist = artist;
+   }
 
+   public int getTotalSong() {
+      return this.totalSong;
+   }
+
+   public void setTotalSong(int totalSong) {
+      this.totalSong = totalSong;
+   }
+
+   public String getLabel() {
+      return this.label;
+   }
+
+   public void setLabel(String label) {
+      this.label = label;
+   }
+
+   public CD() {
+      number = 10;
+      name = "CD Selamat Siang";
+      quantity = 0;
+      artist = "Salman";
+      totalSong = 0;
+      label = "sony music";
+   } 
+
+   public CD(int number, String name, int quantity, double price, String artist, int totalSong, String label) {
+      this.number = number;
+      this.name = name;
+      this.quantity = quantity;
+      this.price = price;
+      this.artist = artist;
+      this.totalSong = totalSong;
+      this.label = label;
+   }
+
+   public void print() {
+      System.out.println("Number : " + number); // atau bisa langsung menggunakan code super.print(); dan bawahnya
+                                                // diberi systemoutprint (agar tidsk error jika super class diganti
+                                                // variable)
+      System.out.println("Name : " + name);
+      System.out.println("Quantitiy : " + quantity);
+      System.out.println("Artist: " + artist);
+      System.out.println("Total song : " + totalSong);
+      System.out.println("Label : " + label);
+   }
+
+}
--- DVD.java
+++ DVD.java
@@ -5,16 +5,51 @@ public class DVD extends Product{
     private String rating;
     private String studio;
 
+    public int getLegth() {
+        return this.legth;
+    }
+
+    public void setLegth(int legth) {
+        this.legth = legth;
+    }
+
+    public String getRating() {
+        return this.rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getStudio() {
+        return this.studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
     public DVD(){
         number = 9;
         name = "DVD Selamat siang";
+        
         quantity = 0;
         price = 0.0;
         legth = 2;
         rating = "remaja";
-        studio = "max picture";
+        studio = "max picture"; 
+    }
 
-        
+    public DVD(int number, String name, int quantity, int price, int legth, String rating, String studio){
+        this.number = number;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.legth = legth;
+        this.rating = rating;
+        this.studio = studio;
+
+
     }
     public void print(){
         System.out.println("Number : " + number);
